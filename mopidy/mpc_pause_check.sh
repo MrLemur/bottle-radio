e54ec087db11f14c4ae1880c3f82725beac1ecfa
Kassim <kassim@kass.im>
2020-05-07 07:52:25 +0000

    Update the check to only work if mpc is playing

--- mopidy/mpc_pause_check.sh
+++ mopidy/mpc_pause_check.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-MPC_CURRENT_TIME=`mpc status | awk '/^\[playing\]/ { sub(/\/.+/,"",$3); split($3,a,/:/); print a[1]*60+a[2] }'`
+if mpc | grep playing;
+then
+	MPC_CURRENT_TIME=`mpc status | awk '/^\[playing\]/ { sub(/\/.+/,"",$3); split($3,a,/:/); print a[1]*60+a[2] }'`
 
-sleep 10
+	sleep 10
 
-MPC_NEW_TIME=`mpc status | awk '/^\[playing\]/ { sub(/\/.+/,"",$3); split($3,a,/:/); print a[1]*60+a[2] }'`
+	MPC_NEW_TIME=`mpc status | awk '/^\[playing\]/ { sub(/\/.+/,"",$3); split($3,a,/:/); print a[1]*60+a[2] }'`
 
-if [ "$MPC_CURRENT_TIME" == "$MPC_NEW_TIME" ]; then mpc next; fi
+	if [ "$MPC_CURRENT_TIME" == "$MPC_NEW_TIME" ]; then mpc next; fi
+fi
