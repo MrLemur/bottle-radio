83422bba469880394933884747d68e10abf3475c
Kassim Benhaddad <kassim@kass.im>
2021-02-28 16:21:19 +0000

    Initial commit

--- start.sh
+++ start.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+echo "Checking for dependencies..."
+if ! which htpasswd; then
+    echo "Missing htpasswd, exiting..."
+    exit 1
+fi
+
 echo "Pulling the latest images..."
 docker-compose pull
 
 echo
 echo "Starting the containers in the background ..."
+docker-compose build frontend
+docker-compose build mopidy
+docker-compose build icecast
 docker-compose up -d
 
 echo
